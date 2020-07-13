1b5395cb4b9a4377ad3110d3ee95af3ac06bf77a
fancylukanator <lukesteenge@gmail.com>
2020-07-13 12:05:15 -0400

    testing node child serial communication with temp36, sending to web #6

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const {spawn} = require('child_process');
+const getCachedSensorReading = require('./getCachedTemp36')
 const port = 3000
 
 app.use('/public', express.static(path.join(__dirname, 'public')))
