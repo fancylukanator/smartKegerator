8c78a918d4dd9f8a53deeeb4a9723a853aa8873e
fancylukanator <lukesteenge@gmail.com>
2020-07-13 12:38:36 -0400

    testing node child serial communication with temp36

--- server/index.js
+++ server/index.js
@@ -6,8 +6,15 @@ const port = 3000
 
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
-app.get('/temperature', function(req, res) {      
-    res.send(getCachedSensorReading.getTemperature().toFixed(1))
+app.get('/temperature', (req, res) => {      
+    const { spawn } = require('child_process');
+    const pyProg = spawn('python', ['./serialTemp.py']);
+
+    pyProg.stdout.on('data', function(data) {
+        console.log(data.toString());
+        res.write(data);
+        res.end('end');
+    });
 })
 
 app.listen(port, () => console.log(`Smart Kegerator listening on port 
