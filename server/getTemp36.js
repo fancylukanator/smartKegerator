1030b7f335fc50221872a821b544dffbbe776673
fancylukanator <lukesteenge@gmail.com>
2020-07-13 12:12:55 -0400

    testing node child serial communication with temp36, sending to web #6

--- server/getTemp36.js
+++ server/getTemp36.js
@@ -1,17 +1,17 @@
 const path = require('path')
 const {spawn} = require('child_process');
 
-/**
- * Run python script, pass in `-u` to not buffer console output 
- * @return {ChildProcess}
- */
-function runScript(){
-  return spawn('python', [
-    "-u", 
-    path.join(__dirname, 'serialTemp36.py')
-  ]);
+const getSensorReading = (callback) => {
+  function runScript(){
+    return spawn('python', [
+      "-u", 
+      path.join(__dirname, 'serialTemp36.py')
+    ]);
+  callback(null, temperature)
+  }
+  const subprocess = runScript()
 }
 
-const subprocess = runScript()
+
 
 module.exports = getSensorReading
