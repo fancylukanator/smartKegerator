96a87164184167aacd8357b83d7765c82fdaf5b7
fancylukanator <lukesteenge@gmail.com>
2020-07-15 16:58:13 -0400

    this is were we make progress

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-const {spawn} = require('child_process');
 
 //import data API
 //const { toggle } = require('./getData')
@@ -9,17 +8,21 @@ const {spawn} = require('child_process');
 //serve index.html at localhost:3000
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
-    //get data
-    var dataToSend;
-    const python = spawn('python', ['serialData.py']);
-    python.stdout.on('data', function (data) {
-        console.log('Pipe data from python script ...');
-        dataToSend = data.toString();
-    });
-    python.on('close', (code) => {
-        console.log(`child process close all stdio with code`);
-    });
 });
+
+//get data?
+app.get('/', (req, res) => {
+
+    const { spawn } = require('child_process');
+    const pyProg = spawn('python', ['./serialData.py']);
+
+    pyProg.stdout.on('data', function(data) {
+
+        console.log(data.toString());
+        res.write(data);
+        res.end('end');
+    });
+})
 
 
 //listen for connection
@@ -27,7 +30,7 @@ io.on('connection', (socket) => {
     console.log('Connected');
     //now send the data
     socket.emit('message', {'message': 'hello world'});
-    socket.emit('data', '${data}');
+    socket.emit('data', data);
     //listen for disconnectS
     socket.on('disconnect', () => {
         console.log('Disconnected')
