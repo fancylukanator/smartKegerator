dbfc6e38950738e6ff2cf052428783b73dd93b39
fancylukanator <lukesteenge@gmail.com>
2020-07-15 16:48:43 -0400

    this is were we make progress

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const {spawn} = require('child_process');
 
 //import data API
-const { toggle } = require('./getData')
+//const { toggle } = require('./getData')
 
 //serve index.html at localhost:3000
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+    //get data
+    var dataToSend;
+    const python = spawn('python', ['serialData.py']);
+    python.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...');
+        dataToSend = data.toString();
+    });
+    python.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
 });
 
 
