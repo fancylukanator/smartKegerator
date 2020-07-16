f413b128a05a13b7fea57c62e0433a490201af77
fancylukanator <lukesteenge@gmail.com>
2020-07-16 11:28:46 -0400

    trying to send data to websocket

--- server/index.js
+++ server/index.js
@@ -3,26 +3,12 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 //import data API
-//const { toggle } = require('./getData')
+const { toggle } = require('./getData')
 
 //serve index.html at localhost:3000
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
-
-//get data?
-app.get('/', (req, res) => {
-
-    const { spawn } = require('child_process');
-    const pyProg = spawn('python', ['./serialData.py']);
-
-    pyProg.stdout.on('data', function(data) {
-
-        console.log(data.toString());
-        res.write(data);
-        res.end('end');
-    });
-})
 
 
 //listen for connection
