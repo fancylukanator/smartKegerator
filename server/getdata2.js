1a8d1a212e26dd55e03e4292827aa1929eee8a63
fancylukanator <lukesteenge@gmail.com>
2020-07-16 20:09:08 -0400

    trying to send data to websocket

--- server/getdata2.js
+++ server/getdata2.js
@@ -2,7 +2,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 async function lsExample() {
-  const { stdout, stderr } = await exec('ls');
+  const { stdout, stderr } = await exec('ls', 'serialData.py');
   console.log('stdout:', stdout);
   console.error('stderr:', stderr);
 }
