7a51c48efa8f2f59f0717a206d871de8af63b792
fancylukanator <lukesteenge@gmail.com>
2020-07-16 20:25:44 -0400

    trying to send data to websocket

--- server/getdata2.js
+++ server/getdata2.js
@@ -1,5 +1,34 @@
-var child = require('child_process').execFile('serialData.py'); 
-// use event hooks to provide a callback to execute when data are available: 
+var spawn = require('child_process').spawn;
+var child = spawn('serialData.py');
+
+// You can also use a variable to save the output 
+// for when the script closes later
+var scriptOutput = "";
+
+child.stdout.setEncoding('utf8');
 child.stdout.on('data', function(data) {
-  console.log(data.toString()); 
+    //Here is where the output goes
+
+    console.log('stdout: ' + data);
+
+    data=data.toString();
+    scriptOutput+=data;
+});
+
+child.stderr.setEncoding('utf8');
+child.stderr.on('data', function(data) {
+    //Here is where the error output goes
+
+    console.log('stderr: ' + data);
+
+    data=data.toString();
+    scriptOutput+=data;
+});
+
+child.on('close', function(code) {
+    //Here you can get the exit code of the script
+
+    console.log('closing code: ' + code);
+
+    console.log('Full output of script: ',scriptOutput);
 });
