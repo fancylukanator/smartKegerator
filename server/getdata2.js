f4cecad24ad33f08044d0905e73001dc33405437
fancylukanator <lukesteenge@gmail.com>
2020-07-16 20:23:02 -0400

    trying to send data to websocket

--- server/getdata2.js
+++ server/getdata2.js
@@ -1,4 +1,5 @@
-var child = require('child_process').execFile('serialData.py', function(err, stdout, stderr) { 
-  // Node.js will invoke this callback when process terminates.
-  console.log(stdout); 
-});  
+var child = require('child_process').execFile('serialData.py'); 
+// use event hooks to provide a callback to execute when data are available: 
+child.stdout.on('data', function(data) {
+  console.log(data.toString()); 
+});
