bb06d8212dfcae1daa65a0398a8a18dc046c14ef
fancylukanator <lukesteenge@gmail.com>
2020-07-29 13:52:07 -0400

    trying to fix cors issue

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     next();
   });
 
