b08ae400996f69e66729c84fb5bc3ccd36088c2a
fancylukanator <lukesteenge@gmail.com>
2020-09-02 19:21:59 -0400

    its working on the pi

--- server/controllers/sensor.controller.js
+++ server/controllers/sensor.controller.js
@@ -7,7 +7,7 @@ const Ready = require('@serialport/parser-ready');
 const Readline = require('@serialport/parser-readline');
 const { serialize } = require('v8');
 const { user } = require("../models");
-const port = new SerialPort('dev/ttyACM0', { baudRate: 9600 });       //COM3 for windown /dev/ttyACM0 for Rpi
+const port = new SerialPort('/dev/ttyACM0', { baudRate: 9600 });       //COM3 for windown /dev/ttyACM0 for Rpi
 
 
 
