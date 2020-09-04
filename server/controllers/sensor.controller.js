ea1bf7e94397137528ce615e733d3639fece58f5
fancylukanator <lukesteenge@gmail.com>
2020-09-04 13:09:26 -0400

    cmon

--- server/controllers/sensor.controller.js
+++ server/controllers/sensor.controller.js
@@ -29,126 +29,127 @@ function updateKeg () {
 
 };
 
-function logPour (req) {
-    
-  if (parsedData.Vol1 !=0) {
-      Tap.find({ inUse: true, tapNumber: 1}).
-          then(keg1 => {
-              var cost = keg1[0].price*parsedData.Vol1;  // unit price of keg 1 times units
-              console.log(req.username);
-              const doc = new Log({"user":req.username,"tap":keg1[0]._id,"volume":parsedData.Vol1,"price":cost});  //create log
-              doc.save(function(err, doc) {
-              if (err) return console.error(err);
-              console.log("Keg 1 logged succesfully")
-              });
-              User.findOneAndUpdate({ username: req.username }, {
-                  $inc: {
-                      balance: cost,                      // keep track of users balance
-                      totalVolume: parsedData.Vol1         // keep track of users total volume
-                  },
-                  lastActive: Date.now(),                   // last pour time
-              },
-                  function(err, res) {
-                      if (err) return console.error(err);
-                      console.log("Stats updated succesfully")
-                  });
-          });
-  }
-  if (parsedData.Vol2 !=0) { 
-      Tap.find({ inUse: true, tapNumber: 2}).
-          then(keg2 => {
-              var cost = keg2[0].price*parsedData.Vol2;
-              const doc = new Log({"user":req.username,"tap":keg2[0]._id,"volume":parsedData.Vol2,"price":cost});
-              doc.save(function(err, doc) {
-              if (err) return console.error(err);
-              console.log("Keg 2 logged succesfully")
-              });
-              User.findOneAndUpdate({ username: req.username }, {
-                  $inc: {
-                      balance: cost,
-                      totalVolume: parsedData.Vol2
-                  },
-                  lastActive: Date.now(),
-              },
-                  function(err, res) {
-                      if (err) return console.error(err);
-                      console.log("Stats updated succesfully")
-                  });
-          });
-  }
-};
 
-function updateStats (req) {
-  var today = new Date(),
-      oneDay = ( 1000 * 60 * 60 * 24 ),
-      thirtyDays = new Date( today.valueOf() - ( 30 * oneDay ) )
-
-  console.log("hello from aggregate");
-
-  Log.aggregate([
-      { '$match': {
-        time: {
-          $gte: thirtyDays
-        },
-        user: req.username
-      }},
-      {'$group': {
-        "_id": '$user',
-        "dayVolume": {
-          '$sum': {
-            '$cond': [
-              { "$gt": [
-                { "$subtract": [ "$time", new Date("1970-01-01") ] },
-                new Date().valueOf() - ( 1000 * 60 * 60 * 24 )
-              ]},
-              '$volume',
-              0
-            ]
-          }
-        },
-        "weekVolume": {
-          '$sum': {
-            '$cond': [
-              { "$gt": [
-                { "$subtract": [ "$time", new Date("1970-01-01") ] },
-                new Date().valueOf() - ( 1000 * 60 * 60 * 24 * 7)
-              ]},
-              '$volume',
-              0
-            ]
-          }
-        },
-        "monthVolume": {
-          '$sum': {
-            '$cond': [
-              { "$gt": [
-                { "$subtract": [ "$time", new Date("1970-01-01") ] },
-                new Date().valueOf() - ( 1000 * 60 * 60 * 24 * 30 )
-              ]},
-              '$volume',
-              0
-            ]
-          }
-        }
-      }}
-    ])
-    .then(stats => {
-        console.log(stats)
-        console.log(stats[0]._id, stats[0].dayVolume);
-      User.findOneAndUpdate({ username: stats[0]._id}, {
-          dayVolume: stats[0].dayVolume,
-          weekVolume: stats[0].weekVolume,
-          monthVolume: stats[0].monthVolume
-      },
-          function(err, res) {
-              if (err) return console.error(err);
-              console.log("AGGREGATION WORKED")
-          });
-    })
-
-};
 
 exports.serialSensorData = (req, res) => {
+  function logPour () {
+    
+    if (parsedData.Vol1 !=0) {
+        Tap.find({ inUse: true, tapNumber: 1}).
+            then(keg1 => {
+                var cost = keg1[0].price*parsedData.Vol1;  // unit price of keg 1 times units
+                console.log(req.username);
+                const doc = new Log({"user":req.username,"tap":keg1[0]._id,"volume":parsedData.Vol1,"price":cost});  //create log
+                doc.save(function(err, doc) {
+                if (err) return console.error(err);
+                console.log("Keg 1 logged succesfully")
+                });
+                User.findOneAndUpdate({ username: req.username }, {
+                    $inc: {
+                        balance: cost,                      // keep track of users balance
+                        totalVolume: parsedData.Vol1         // keep track of users total volume
+                    },
+                    lastActive: Date.now(),                   // last pour time
+                },
+                    function(err, res) {
+                        if (err) return console.error(err);
+                        console.log("Stats updated succesfully")
+                    });
+            });
+    }
+    if (parsedData.Vol2 !=0) { 
+        Tap.find({ inUse: true, tapNumber: 2}).
+            then(keg2 => {
+                var cost = keg2[0].price*parsedData.Vol2;
+                const doc = new Log({"user":req.username,"tap":keg2[0]._id,"volume":parsedData.Vol2,"price":cost});
+                doc.save(function(err, doc) {
+                if (err) return console.error(err);
+                console.log("Keg 2 logged succesfully")
+                });
+                User.findOneAndUpdate({ username: req.username }, {
+                    $inc: {
+                        balance: cost,
+                        totalVolume: parsedData.Vol2
+                    },
+                    lastActive: Date.now(),
+                },
+                    function(err, res) {
+                        if (err) return console.error(err);
+                        console.log("Stats updated succesfully")
+                    });
+            });
+    }
+  };
+  
+  function updateStats () {
+    var today = new Date(),
+        oneDay = ( 1000 * 60 * 60 * 24 ),
+        thirtyDays = new Date( today.valueOf() - ( 30 * oneDay ) )
+  
+    console.log("hello from aggregate");
+  
+    Log.aggregate([
+        { '$match': {
+          time: {
+            $gte: thirtyDays
+          },
+          user: req.username
+        }},
+        {'$group': {
+          "_id": '$user',
+          "dayVolume": {
+            '$sum': {
+              '$cond': [
+                { "$gt": [
+                  { "$subtract": [ "$time", new Date("1970-01-01") ] },
+                  new Date().valueOf() - ( 1000 * 60 * 60 * 24 )
+                ]},
+                '$volume',
+                0
+              ]
+            }
+          },
+          "weekVolume": {
+            '$sum': {
+              '$cond': [
+                { "$gt": [
+                  { "$subtract": [ "$time", new Date("1970-01-01") ] },
+                  new Date().valueOf() - ( 1000 * 60 * 60 * 24 * 7)
+                ]},
+                '$volume',
+                0
+              ]
+            }
+          },
+          "monthVolume": {
+            '$sum': {
+              '$cond': [
+                { "$gt": [
+                  { "$subtract": [ "$time", new Date("1970-01-01") ] },
+                  new Date().valueOf() - ( 1000 * 60 * 60 * 24 * 30 )
+                ]},
+                '$volume',
+                0
+              ]
+            }
+          }
+        }}
+      ])
+      .then(stats => {
+          console.log(stats)
+          console.log(stats[0]._id, stats[0].dayVolume);
+        User.findOneAndUpdate({ username: stats[0]._id}, {
+            dayVolume: stats[0].dayVolume,
+            weekVolume: stats[0].weekVolume,
+            monthVolume: stats[0].monthVolume
+        },
+            function(err, res) {
+                if (err) return console.error(err);
+                console.log("AGGREGATION WORKED")
+            });
+      })
+  
+  };
     // Read the port data
     ready = port.pipe(new Ready( { delimiter: '{'}));
     parser = port.pipe(new Readline({ delimiter: '\n' }));
